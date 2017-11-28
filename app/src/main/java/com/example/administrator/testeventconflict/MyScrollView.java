cea45df8a2f37a3ed959c477345285364c904ecf
APOLLO\Administrator <wpb504@126.com>
2017-11-28 13:37:06 +0800

    距离结合速度和点按时间判定父控件滑动事件是否分发

--- app/src/main/java/com/example/administrator/testeventconflict/MyScrollView.java
+++ app/src/main/java/com/example/administrator/testeventconflict/MyScrollView.java
@@ -22,6 +22,9 @@ public class MyScrollView extends ScrollView {
     private float mYStart;
     private float mYEnd;
     private float mYDistance;
+    private long mStartTime;
+    private long mEndTime;
+    private long mTotalTime;
 
     public MyScrollView(Context context) {
         this(context, null);
@@ -47,11 +50,14 @@ public class MyScrollView extends ScrollView {
                 //触点第一个id
                 mPointerId = ev.getPointerId(0);
                 mYStart = ev.getRawY();
+                mStartTime = System.currentTimeMillis();
                 break;
 
             case MotionEvent.ACTION_MOVE:
                 mYEnd = ev.getRawY();
+                mEndTime = System.currentTimeMillis();
                 mYDistance = mYEnd - mYStart;
+                mTotalTime = mEndTime - mStartTime;
                 //求伪瞬时速度
                 verTracker.computeCurrentVelocity(1, (float) 10);//设置maxVelocity值为10时，速率大于10时，显示的速率都是10,速率小于10时，显示正常
                 final float velocityX = verTracker.getXVelocity(mPointerId);
@@ -59,7 +65,9 @@ public class MyScrollView extends ScrollView {
                 Log.d(TAG, "x速度：" + velocityX);
                 Log.d(TAG, "y速度：" + velocityY);
                 Log.d(TAG, "y距离：" + mYDistance);
-                if (Math.abs(velocityY) > 4.5f || Math.abs(mYDistance) > dip2px(mContext,50f)) {
+                Log.d(TAG, "用时：" + mTotalTime);
+
+                if (Math.abs(velocityY) > 4.5f || Math.abs(mYDistance) > dip2px(mContext, 50f) || mTotalTime > 500) {
                     return true;
                 }
 
